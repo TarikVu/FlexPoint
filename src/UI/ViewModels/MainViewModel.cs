26a01ff47159f391467609101b518a5a55a4c9a5
TarikVu <tarikvu2@gmail.com>
2024-11-09 12:20:07 -0700

    Added Tests to ViewModel. Added more Buttons to UI for (currently) available DB parameters. Added Separate Constructors to view model for testing
    added more Articles of reference for MVVM, Databinding, and Relay Command.

--- src/UI/ViewModels/MainViewModel.cs
+++ src/UI/ViewModels/MainViewModel.cs
@@ -12,6 +12,8 @@ namespace UI.ViewModels
     {
         private readonly ExerciseDbApi _exerciseDbApi;
         private ObservableCollection<Exercise> _exercises;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public ICommand FetchExercisesCommand { get; }
 
         public ObservableCollection<Exercise> Exercises
@@ -24,15 +26,33 @@ namespace UI.ViewModels
             }
         }
 
-        public MainViewModel()
+        /// <summary>
+        /// Main Constructor to set up our ExerciseDbApi, exercises, and RelayCommand
+        /// </summary>
+        /// <param name="exerciseDbApi">
+        /// Optional Parameter Used for Tests when mocking the api, used to reduce
+        /// Calling the acutal api in case of rate limitations</param>
+        public MainViewModel(ExerciseDbApi? exerciseDbApi = null)
         {
-            _exerciseDbApi = new ExerciseDbApi(new HttpClient());
-            Exercises = new ObservableCollection<Exercise>();
-            FetchExercisesCommand = new RelayCommand(async () => await FetchExercisesAsync("biceps"));
+            _exerciseDbApi = exerciseDbApi ?? new ExerciseDbApi(new HttpClient());
+            _exercises = new ObservableCollection<Exercise>();
+            FetchExercisesCommand = new RelayCommand(async (muscle) => await FetchExercisesAsync(muscle as string));
         }
 
-        private async Task FetchExercisesAsync(string muscle)
+        // Parameterless constructor for XAML instantiation
+        public MainViewModel() : this(new ExerciseDbApi(new HttpClient()))
         {
+        }
+
+        /// <summary>
+        /// Calls API to fetch Exercises.
+        /// </summary>
+        /// <param name="muscle"></param>
+        /// <returns></returns>
+        private async Task FetchExercisesAsync(string? muscle)
+        {
+            if (string.IsNullOrEmpty(muscle))
+                return;
             try
             {
                 var exerciseList = await _exerciseDbApi.GetExercisesAsync(muscle);
@@ -48,9 +68,7 @@ namespace UI.ViewModels
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
--- src/UI/ViewModels/RelayCommand.cs
+++ src/UI/ViewModels/RelayCommand.cs
@@ -4,22 +4,16 @@ using System.Windows.Input;
 
 namespace UI.ViewModels
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand(Func<object?, Task> execute, Func<bool>? canExecute = null) : ICommand
     {
-        private readonly Func<Task> _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly Func<object?, Task> _execute = execute;
+        private readonly Func<bool>? _canExecute = canExecute;
 
-        public RelayCommand(Func<Task> execute, Func<bool> canExecute = null)
-        {
-            _execute = execute;
-            _canExecute = canExecute;
-        }
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
 
-        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+        public async void Execute(object? parameter) => await _execute(parameter);
 
-        public async void Execute(object parameter) => await _execute();
-
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged;
 
         public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
