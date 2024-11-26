0489595b93f7f586eb6f73e60a9c3f4b25a8920e
TarikVu <tarikvu2@gmail.com>
2024-11-26 11:00:34 -0700

    New User adds a new user to sqlite db

---
+++ src/Backend/FlexPointDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class FlexPointDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=users.db");
+    }
+}
